5b191da1112d0d4f2269dd275f3930fd097e63c4
hasung98 <james981020@gmail.com>
2021-04-22 01:20:01 +0900

    loadData

--- manager.c
+++ manager.c
@@ -55,10 +55,17 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
-
-
-
-
+	fp = fopen("product.txt","rt");
+	if(fp == NULL) printf("파일없음");
+	else {
+		for(count; count < 100; count++){
+			fscanf(fp,"%d", &p[count].weight);
+			if(feof(fp)) break;
+			fscanf(fp,"%d", &p[count].price);
+			fscanf(fp,"%[^\n]s", p[count].name);
+		}
+	fclose(fp);
 	printf("=> 로딩 성공!\n");
 	return count;
+	}
 }
